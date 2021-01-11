4dab0d066a168fa3893c69091105a04b41588b9b
xujianwen <xujianwen@miaozhen.com>
2021-01-11 18:11:49 +0800

    初试

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
+// React 负责逻辑控制： 数据 -> VDOM
 import React from 'react';
+// ReactDOM 负责渲染真实的 DOM: vdom -> dom
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// React 17 以前的版本使用 JSX 的时候必须引入 React ,否则会报错
+// 因为 JSX 是 React.createElement(component, props, ...children) 的语法糖
+// JSX 会编译为 React.createElement 调用形式，所以 React 库也必须包含在 JSX 代码作用域内。
 ReactDOM.render(
   <React.StrictMode>
     <App />
