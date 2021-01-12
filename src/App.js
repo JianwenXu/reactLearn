fb4f8e7e2ec98cad1cf17eee9a1268cb91f7a2a8
xujianwen <xujianwen@miaozhen.com>
2021-01-12 10:01:31 +0800

    初识2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
+// JSX 里面渲染一个数组的时候必须有 key
+// diff 的时候，首先比较 type ,然后是 key, 所以同级同类型元素，key 值必须得是惟一的
 function App() {
   return (
     <div className="App">
